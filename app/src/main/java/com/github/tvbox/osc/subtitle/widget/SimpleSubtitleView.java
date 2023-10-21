39758f02739c28cea2e75a0df012ef4be717adba
watson1982 <112817572+watson1982@users.noreply.github.com>
2023-10-22 01:20:08 +0800

    修正字幕乱码问题

--- app/src/main/java/com/github/tvbox/osc/subtitle/widget/SimpleSubtitleView.java
+++ app/src/main/java/com/github/tvbox/osc/subtitle/widget/SimpleSubtitleView.java
@@ -70,8 +70,10 @@ public class SimpleSubtitleView extends TextView
         String text = subtitle.content;
         text = text.replaceAll("(?:\\r\\n)", "<br />");
         text = text.replaceAll("(?:\\r)", "<br />");
-        text = text.replaceAll("(?:\\n)", "<br />");
-        text = text.replaceAll("\\{[\\s\\S]*\\}", "");
+        text = text.replaceAll("(?:\\n)", "<br />");        
+        text = text.replaceAll("\\\\N", "<br />");
+        text = text.replaceAll("\\{[\\s\\S]*?\\}", "");
+        text = text.replaceAll("^.*?,.*?,.*?,.*?,.*?,.*?,.*?,.*?,.*?,", "");    
         setText(Html.fromHtml(text));
     }
 
