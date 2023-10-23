9c3b051a2b647eccc1c78e747b6e444b2ae80516
chengxue2020 <65702849+chengxue2020@users.noreply.github.com>
2023-10-23 22:53:10 +0800

    改部分词到string文件

--- app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
@@ -694,7 +694,7 @@ public class LivePlayActivity extends BaseActivity {
                                 tv_next_name.setText(((Epginfo) arrayList.get(size + 1)).title);
                             } else {
                                 tv_next_time.setText("00:00 - 23:59");
-                                tv_next_name.setText("No Information");
+                                tv_next_name.setText(getString(R.string.act_no_EPG_information));
                             }
                             break;
                         } else {
