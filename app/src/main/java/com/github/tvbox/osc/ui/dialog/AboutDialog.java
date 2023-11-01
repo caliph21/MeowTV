5de21aef5499bcaf02edd74ed83cbe39e6fbb481
chengxue2020 <65702849+chengxue2020@users.noreply.github.com>
2023-11-01 16:19:13 +0800

    增加在线更新功能

--- app/src/main/java/com/github/tvbox/osc/ui/dialog/AboutDialog.java
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/AboutDialog.java
@@ -1,17 +1,35 @@
 package com.github.tvbox.osc.ui.dialog;
 
 import android.content.Context;
+import android.widget.TextView;
+import android.os.Bundle;
+import com.github.tvbox.osc.BuildConfig;
 
 import androidx.annotation.NonNull;
 
 import com.github.tvbox.osc.R;
 
 import org.jetbrains.annotations.NotNull;
+import com.github.tvbox.osc.ui.xupdate.Constants;
+import com.xuexiang.xupdate.easy.EasyUpdate;
 
 public class AboutDialog extends BaseDialog {
 
     public AboutDialog(@NonNull @NotNull Context context) {
         super(context);
         setContentView(R.layout.dialog_about);
+		findViewById(R.id.xupdate).setOnClickListener(view -> {
+            switch (view.getId()) {
+                case R.id.xupdate:
+                    EasyUpdate.checkUpdate(getContext(), Constants.UPDATE_DEFAULT_URL);
+                    break;
+                                }
+        });
+    }
+    private TextView appVersion;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        appVersion = (TextView)findViewById(R.id.app_version);
+        appVersion.setText(BuildConfig.VERSION_NAME);
     }
 }
---
+++ app/src/main/java/com/github/tvbox/osc/ui/xupdate/Constants.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2020 xuexiangjys(xuexiangjys@163.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.github.tvbox.osc.ui.xupdate;
+
+/**
+ * @author xuexiang
+ * @since 2020/12/20 1:58 PM
+ */
+public final class Constants {
+
+    public static final String UPDATE_DEFAULT_URL = "https://chengxue2020.github.io/Cat-ports/App/tk-update.json";
+
+    public static final String UPDATE_FORCE_URL = "https://chengxue2020.github.io/Cat-ports/App/tk-update_forced.json";
+
+    public static final String UPDATE_DOWNLOAD_URL = "https://chengxue2020.github.io/Cat-ports/App/tk-download_test.json";
+}
