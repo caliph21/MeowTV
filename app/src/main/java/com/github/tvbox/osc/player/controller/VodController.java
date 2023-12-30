a79eb2719303829ce33028a6e350ce84acf9cb74
watson1982 <112817572+watson1982@users.noreply.github.com>
2023-12-30 21:18:48 +0800

    code

--- app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
+++ app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
@@ -137,9 +137,9 @@ public class VodController extends BaseController {
 
                         // takagen99: Check if Touch Screen, show back button
                         if (((BaseActivity) mActivity).supportsTouch()) {
-                            mBack.setVisibility(VISIBLE);
-                            showLockView();
+                            mBack.setVisibility(VISIBLE);               
                         }
+                        showLockView();
 
                         if (isKeyUp) {
                             mPlayerTimeStartBtn.requestFocus();
@@ -250,10 +250,16 @@ public class VodController extends BaseController {
     // center BACK button
     LinearLayout mBack;
     
-    //center LOCK button
+    //center LOCK button    
     private boolean isLock = false;
     ImageView mLockView;
     LockRunnable lockRunnable = new LockRunnable();
+    
+    // screen_display
+    TextView mPlayLoadNetSpeedRightTop;
+    LinearLayout mTopRoot2;
+    TextView seekTime; //右上角进度时间显示
+    LinearLayout mScreendisplay; //增加屏显开关
 
     // bottom container
     LinearLayout mBottomRoot;
@@ -308,6 +314,8 @@ public class VodController extends BaseController {
         public void run() {
             Date date = new Date();
             SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm aa", Locale.ENGLISH);
+            String speed = PlayerHelper.getDisplaySpeed(mControlWrapper.getTcpSpeed());
+            mPlayLoadNetSpeedRightTop.setText(speed);
             mTime.setText(timeFormat.format(date));
             mHandler.postDelayed(this, 1000);
         }
@@ -414,6 +422,12 @@ public class VodController extends BaseController {
 
         // initialize subtitle
         initSubtitleInfo();
+        
+        // screen_display
+        mPlayLoadNetSpeedRightTop = findViewById(R.id.tv_play_load_net_speed_right_top);
+        mTopRoot2 = findViewById(R.id.tv_top_r_container);
+        seekTime = findViewById(R.id.tv_seek_time); //右上角进度时间显示
+        mScreendisplay = findViewById(R.id.screen_display); //增加屏显开关
         
         mLockView.setOnClickListener(new OnClickListener() {
             @Override
@@ -932,6 +946,18 @@ public class VodController extends BaseController {
                 }
             }
         });
+        
+        //屏显开关
+        mTopRoot2.setVisibility(Hawk.get(HawkConfig.SCREEN_DISPLAY, GONE));
+        mScreendisplay.setOnClickListener(new OnClickListener() {@Override
+            public void onClick(View view) {
+                mTopRoot2.setVisibility(mTopRoot2.getVisibility() == VISIBLE ? GONE : VISIBLE);
+                Hawk.put(HawkConfig.SCREEN_DISPLAY, mTopRoot2.getVisibility());              
+                hideBottom();
+                //Toast.makeText(getContext(), "点击显示网速 播放进度 时间", Toast.LENGTH_SHORT).show();
+            }
+        });
+        
     }
     
     public void initLandscapePortraitBtnInfo() {
@@ -1075,6 +1101,7 @@ public class VodController extends BaseController {
         }
         mCurrentTime.setText(PlayerUtils.stringForTimeVod(position));
         mTotalTime.setText(PlayerUtils.stringForTimeVod(duration));
+        seekTime.setText((PlayerUtils.stringForTime(position)) + " | " + (PlayerUtils.stringForTime(duration))); //右上角进度条时间显示
         if (duration > 0) {
             mSeekBar.setEnabled(true);
             int pos = (int) (position * 1.0 / duration * mSeekBar.getMax());
