75cd0c395d929cde06e9f5b7298723e1f4616e8a
zhixc <93297256+zhixc@users.noreply.github.com>
2024-01-11 12:03:41 +0000

    退出直播界面后，epg图片请求完成加载引起闪退修复，直播界面画中画判定调整

--- app/src/main/java/com/github/tvbox/osc/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -96,7 +96,7 @@ public class App extends MultiDexApplication {
         // 播放器选项
         putDefault(HawkConfig.SHOW_PREVIEW, true);           //窗口预览: true=开启, false=关闭
         putDefault(HawkConfig.PLAY_SCALE, 0);                //画面缩放: 0=默认, 1=16:9, 2=4:3, 3=填充, 4=原始, 5=裁剪
-        putDefault(HawkConfig.PIC_IN_PIC, true);             //画中画: true=开启, false=关闭
+        putDefault(HawkConfig.BACKGROUND_PLAY_TYPE, 0);      //后台：0=关闭, 1=开启, 2=画中画
         putDefault(HawkConfig.PLAY_TYPE, 1);                 //播放器: 0=系统, 1=IJK, 2=Exo, 3=MX, 4=Reex, 5=Kodi
         putDefault(HawkConfig.IJK_CODEC, "硬解码");           //IJK解码: 软解码, 硬解码
         // 系统选项
